a8438d01b04cb99c530b7736ed58f9cd3b183539
Abhishek Sinha <email4abhi@gmail.com>
2021-10-05 21:23:27 +0530

    Added Tesseract Header files

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
+#include <tesseract/baseapi.h>
+#include <leptonica/allheaders.h>
 
 using namespace std;
 
 int main()
 {
 	cout << " Tesseract OCR Example Code :- " << endl;
-	
+
 	return 0;
 }
